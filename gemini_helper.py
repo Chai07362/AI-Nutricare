9abfddc933e3b2d6b4bcc8bd71acb90f9966f2a0
Chai07362 <chaitanyaukarande07@gmail.com>
2025-12-25 23:54:18 +0530

    Update gemini_helper.py

--- gemini_helper.py
+++ gemini_helper.py
@@ -2,11 +2,12 @@ import streamlit as st
 import google.generativeai as genai
 
 # Configure Gemini API key
-genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 # Create Gemini model once
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def get_gemini_response(input_prompt, image_parts):
     response = model.generate_content([input_prompt, image_parts[0]])
     return response.text
+
